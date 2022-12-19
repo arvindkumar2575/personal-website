e7b13b3e5c2075464c81d1fff9e2fb0ba31b3dbd
Arvind Kumar <arvindkumar2575@gmail.com>
2022-12-19 23:00:52 +0530

    add files changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {dbjson} = require('./src/data')
 const port = process.env.PORT || 8080
 
 //css,js,images files assets
@@ -12,8 +13,11 @@ app.set('views', path.join(__dirname, 'views'))
 
 //routes
 app.get('/', (req, res) => {
-    console.log(__dirname)
-    res.render('index')
+    // console.log(dbjson)
+    let data = {};
+    data.title=dbjson.title
+    data.skills=dbjson.skills
+    res.render('index',{'data':data})
 })
 
 app.get('/about-us', (req, res) => {
--- src/data.js
+++ src/data.js
@@ -1,8 +1,18 @@
-const data = {
+const dbjson = {
+    title:"Arvind Kumar | Portfolio",
     navbar:{
         status:true,
         left:{
             site_name:"Arvind Kumar"
         }
+    },
+    skills:{
+        "Java":["0F6B2D",""],
+        "JavaScript":["0F6B6B",""],
+        "Php":["6B0F4C",""],
+        "Python":["6B2E0F",""],
+        "HTML":["6B5C0F",""],
+        "CSS":["36373C",""],
     }
 }
+module.exports = {dbjson}
